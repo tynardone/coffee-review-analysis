5f2fb1180933411bb86da3194b9fcc215374b13e
Tyler Nardone <tynardone@gmail.com>
2024-11-04 14:35:02 -0500

    Added logging

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,4 @@
 from datetime import datetime
-from pathlib import Path
 
 
 def create_filename(filename: str, filetype: str) -> str:
@@ -7,13 +6,3 @@ def create_filename(filename: str, filetype: str) -> str:
     current_date: str = datetime.now().strftime("%d%m%Y")
     filename = f"{current_date}_{filename}.{filetype}"
     return filename
-
-
-def create_filepath(filename: str, filetype: str) -> Path:
-    """Creates a filepath for the file in the 'data/raw' directory."""
-    if filetype not in ("csv", "json", "pkl"):
-        raise ValueError(
-            "Invalid file type. Only 'csv', 'json', and 'pkl' are supported."
-        )
-    data_dir = Path("data/raw/")
-    return data_dir / create_filename(filename, filetype)
