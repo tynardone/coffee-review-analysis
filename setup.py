839f3509ae5e1af8ddeaaafa71d615858c33daee
Tyler Nardone <tynardone@gmail.com>
2024-11-06 15:18:28 -0500

    Setting up project for install

--- setup.py
+++ setup.py
@@ -1,3 +1,7 @@
 from setuptools import find_packages, setup
 
-setup(name="coffee-review", version="0.1", packages=find_packages())
+setup(
+    name="coffee-review",
+    version="0.1",
+    packages=find_packages(),
+)
