dca595d9a9260b50a138fb5d08849a7e988cd655
Tyler Nardone <tynardone@gmail.com>
2024-12-14 10:49:11 -0500

    Finished splitting logic up

--- log.py
+++ log.py
@@ -1,21 +1,22 @@
+# type: ignore
 import atexit
 import json
 import logging
+import logging.config
+from logging.handlers import QueueHandler
 
 CONFIG_FILE = ""
 
 logger = logging.getLogger("my_app")
 
 
-def setup_logging():
+def setup_logging() -> None:
     if not CONFIG_FILE:
         raise ValueError("Config file path is not set.")
     with open(CONFIG_FILE) as f_in:
         config = json.load(f_in)
     logging.config.dictConfig(config)
-    queue_handler: logging.handlers.QueueHandler = logging.getHandlerByName(
-        "queue_handler"
-    )
-    if queue_handler is not None:
+    queue_handler: QueueHandler | None = logging.getHandlerByName("queue_handler")
+    if queue_handler:
         queue_handler.listener.start()
         atexit.register(queue_handler.listener.stop)
