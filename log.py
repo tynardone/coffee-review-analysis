f700df6aa4d483679b294c5608d6a94f1f41ef6d
Tyler Nardone <tynardone@gmail.com>
2024-12-17 10:36:38 -0500

    added try except block to log.py

--- log.py
+++ log.py
@@ -4,21 +4,27 @@ import logging
 import logging.config
 from logging.handlers import QueueHandler
 
-CONFIG_FILE: str = ""
+CONFIG_FILE: str = "loggingconfig.json"
 
 logger = logging.getLogger("my_app")
 
 
 def setup_logging() -> None:
     """
-    Load dict config for logging.
+    Load dictionary configuration for logging and initialize queue handler listener.
     """
     if not CONFIG_FILE:
         raise ValueError("Config file path is not set.")
+
     with open(CONFIG_FILE) as f_in:
         config = json.load(f_in)
+
     logging.config.dictConfig(config)
-    queue_handler: QueueHandler | None = logging.getHandlerByName("queue_handler")  # type: ignore
-    if queue_handler:
-        queue_handler.listener.start()  # type: ignore
-        atexit.register(queue_handler.listener.stop)  # type: ignore
+    handler_name: str = "queue_handler"
+    handler = logging.getHandlerByName(handler_name)
+    if isinstance(handler, QueueHandler):
+        # `listener` not recognized by mypy but is valid at runtime
+        handler.listener.start()  # type: ignore[attr-defined]
+        atexit.register(handler.listener.stop)  # type: ignore[attr-defined]
+    else:
+        raise TypeError(f"Handler {handler_name} not loaded from config properly")
