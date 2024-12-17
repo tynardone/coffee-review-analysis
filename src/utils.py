e81934ddf991f28e83df5356373f3ed489cc7ba1
Tyler Nardone <tynardone@gmail.com>
2024-12-17 09:26:03 -0500

    Added docstring and arg check to create_filename

--- src/utils.py
+++ src/utils.py
@@ -2,7 +2,15 @@ from datetime import datetime
 
 
 def create_filename(filename: str, filetype: str) -> str:
-    """Creates a filename of format 'ddmmyyyy_filename.filetype'"""
+    """
+    Creates a filename of format 'ddmmyyyy_filename.filetype
+
+    Args:
+        filename (str): Name of file after date prefix
+        filetype (str): Type of file to save
+    '"""
+    if filetype not in ("csv", "json"):
+        raise ValueError("filetype must be one of: csv, json")
     current_date: str = datetime.now().strftime("%d%m%Y")
     filename = f"{current_date}_{filename}.{filetype}"
     return filename
