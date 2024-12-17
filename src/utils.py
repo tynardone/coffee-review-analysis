4c14286c092a503d1dcaf77723e0df07720acc2a
Tyler Nardone <tynardone@gmail.com>
2024-12-17 09:27:27 -0500

    Updated docstring for create_filename

--- src/utils.py
+++ src/utils.py
@@ -8,6 +8,10 @@ def create_filename(filename: str, filetype: str) -> str:
     Args:
         filename (str): Name of file after date prefix
         filetype (str): Type of file to save
+
+    Returns:
+        filename (str): Returns a filename of form
+                        {current date ddmmyyyy}_{filename}.{filetype}
     '"""
     if filetype not in ("csv", "json"):
         raise ValueError("filetype must be one of: csv, json")
